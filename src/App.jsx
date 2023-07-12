8cfd34f0c35b613976ba7a1f035447e7f3240866
andressafirmino <andressafirmino78@gmail.com>
2023-07-12 14:57:01 -0300

    Versão com merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { styled } from "styled-components";
-
+import HomePage from "./pages/HomePage"
+import CartPage from "./pages/CartPage/CartPage";
 
 function App() {
   return (
