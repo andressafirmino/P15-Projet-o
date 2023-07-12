ca7319ed3535167cbd27d206b8694e574930c148
andressafirmino <andressafirmino78@gmail.com>
2023-07-12 15:04:16 -0300

    Ajuste

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 import { styled } from "styled-components";
-import HomePage from "./pages/HomePage"
 import CartPage from "./pages/CartPage/CartPage";
+import HomePage from "./pages/HomePage/HomePage";
 
 function App() {
   return (
