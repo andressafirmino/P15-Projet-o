08cb9b0ae44c3dcbb3e0a7ab3e0bd3f04c9a0b9a
THIAGO SOUSA <thiago21sousa21@gmail.com>
2023-07-12 13:21:13 -0300

    global style

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,8 +4,22 @@ export default function HomePage() {
 
     return (
         <CsHomePage>
-            <header></header>
-            <main>adsf</main>
+            <header>
+                <div className="logo">LOGO</div>
+                <div className="sectors">SALA COZINHA ESCRITÓRIO</div>
+                <div className="interaction"></div>
+            </header>
+            <main>
+                <div className="product"></div>
+                <div className="product"></div>
+                <div className="product"></div>
+                <div className="product"></div>
+                <div className="product"></div>
+                <div className="product"></div>
+                <div className="product"></div>
+                <div className="product"></div>
+
+            </main>
         </CsHomePage>
     );
 
@@ -20,17 +34,51 @@ const CsHomePage = styled.div`
   display: flex;
   flex-direction: column;
 
+    *{
+        border: 1px solid;
 
+    }
 
   header{
     width: 100%;
-    height: 40px;
+    height: 80px;
     background-color: blue;
+    display: flex;
+    justify-content: space-between;
+
+    .logo{
+        height: 100%;
+        width: 200px;
+        font-size: 50px;
+    }
+    .sectors{
+        height: 100%;
+        width: 500px;
+        font-size: 30px;
+
+    }
+    .interaction{
+        height: 100%;
+        width: 300px;
+    }
   }
+
   main{
     width: 100%;
     height: auto;
     border: 1px solid;
+
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    padding: 30px;
+
+    .product{
+        width: 200px;
+        height: 300px;
+        margin: 10px;
+
+    }
 
   }
 `;
