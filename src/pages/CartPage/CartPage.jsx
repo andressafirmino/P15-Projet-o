8b414b60d4d15da30c9fd2a5dc5fda639c42076a
andressafirmino <andressafirmino78@gmail.com>
2023-07-13 02:07:55 -0300

    feat/subtotal habilitado

--- src/pages/CartPage/CartPage.jsx
+++ src/pages/CartPage/CartPage.jsx
@@ -1,62 +1,40 @@
+import { useState } from "react";
 import { styled } from "styled-components";
+import CartProducts from "./CartProductsPage/CartProducts";
 
 export default function CartPage() {
 
+    
     const cartItem = [
         {
             id: 1,
             name: "mesa",
             image: "https://www.larshopping.com.br/media/catalog/product/cache/1/image/1200x/9df78eab33525d08d6e5fb8d27136e95/w/h/whatsapp_image_2022-03-30_at_11.40.16_1_.jpeg",
-            value: "R$ 3.000,00",
+            value: "3.000,99",
             description: "Mesa com 6 lugares"
         }
     ]
 
+    
     return (
         <CartContainer>
-                <CartHeader>
-                    <p>Produtos</p>
-                    <div>
-                        <div>Preço unitário</div>
-                        <div>Quantidade</div>
-                        <div>Preço Total</div>
-                    </div>
-                </CartHeader>
-                    <ItemList >
-                        <div>
-                            <img  />
-                            <InfoItem>
-                                <p>Nome</p>
-                                <p>Descrição</p>
-                            </InfoItem>
-                        </div>
-                        <div>
-                            <Counter>
-                                <div>
-                                    <p>R$</p>
-                                </div>
-                                <CounterButton>
-                                    <div>
-                                        <button>-</button>
-                                    </div>
-                                    <div>10</div>
-                                    <div>
-                                        <button>+</button>
-                                    </div>
-                                </CounterButton>
-                                <div>
-                                    <p>R$Total</p>
-                                </div>
-                            </Counter>
-                        </div>
-                    </ItemList>
-                
+            <CartHeader>
+                <p>Produtos</p>
+                <div>
+                    <div>Preço unitário</div>
+                    <div>Quantidade</div>
+                    <div>Preço Total</div>
+                </div>
+            </CartHeader>
+            {cartItem.map(item => (<CartProducts key={item.id} 
+            name={item.name} image={item.image}
+            value={item.value} description={item.description}/>))}          
         </CartContainer>
     )
 }
 
 const CartContainer = styled.div`
-    width: 100vw;
+    width: 100%;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -64,11 +42,10 @@ const CartContainer = styled.div`
 `
 
 const CartHeader = styled.div`
-    width: calc(100vw - 40px);
+    width: 100%;
     height: 60px;
     border: 1px solid #000000;
     border-radius: 5px;
-    padding: 10px;
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -94,67 +71,4 @@ const CartHeader = styled.div`
     }
     }
     
-`
-const ItemList = styled.div`
-    width: calc(100vw - 40px);
-    height: 80px;
-    border: 1px solid #000000;
-    border-radius: 5px;
-    padding: 10px;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    div {
-        display: flex;
-        justify-content: flex-start;
-        align-items: center;
-        img {
-        width: 60px;
-        height: 60px;
-        border: 1px solid #000000;
-        border-radius: 5px;
-        margin-right: 10px;
-        }        
-    }    
-`
-
-const InfoItem = styled.div`
-    display: flex;
-    flex-direction: column;
-    justify-content: flex-start;
-    align-items: flex-start;
-    p {
-        font-size: 18px;
-        font-weight: 400;
-        color: #000000;
-    }
-`
-const Counter = styled.div`
-    border: 1px solid #000000;
-    border-radius: 5px;
-    display: flex;
-    justify-content: flex-end;
-    div {
-        width: 200px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        margin-left: 10px;
-        p {
-            font-size: 20px;
-            font-weight: 700;
-            color: #000000;
-        }
-        
-    }
-`
-const CounterButton = styled.div`
-    display: flex;
-    justify-content: center;
-    div {
-        width: 20px;
-        height: 20px;
-        background-color: lightgray;
-        text-align: center;
-    }
 `
---
+++ src/pages/CartPage/CartProductsPage/CartProducts.jsx
@@ -0,0 +1,120 @@
+import { useState } from "react";
+import { styled } from "styled-components";
+
+export default function CartProducts(props) {
+    const { name, description, value, image } = props;
+    const [counter, setCounter] = useState(1);
+    const [subtotal, setSubtotal] = useState(0);
+    
+    function minus() {
+        setCounter(counter => counter - 1);
+        let numberFormat = parseFloat(value.replace(/\./g, "").replace(",", ".")).toFixed(2);
+        let mult = counter * numberFormat;
+        let subtotalWithSeparator = mult.toLocaleString('pt-BR');
+        setSubtotal(subtotalWithSeparator);
+    }
+    function plus() {
+        setCounter(counter => counter + 1);
+        let numberFormat = parseFloat(value.replace(/\./g, "").replace(",", ".")).toFixed(2);
+        let mult = counter * numberFormat;
+        let subtotalWithSeparator = mult.toLocaleString('pt-BR');
+        setSubtotal(subtotalWithSeparator);
+        
+    }
+
+    return (
+        <ItemList >
+            <div>
+                <img src={image}/>
+                <InfoItem>
+                    <p>{name}</p>
+                    <p>{description}</p>
+                </InfoItem>
+            </div>
+            <div>
+                <Counter>
+                    <div>
+                        <p>R${value}</p>
+                    </div>
+                    <CounterButton>
+                        <div>
+                            <button onClick={minus}>-</button>
+                        </div>
+                        <div>{counter}</div>
+                        <div>
+                            <button onClick={plus}>+</button>
+                        </div>
+                    </CounterButton>
+                    <div>
+                        <p>R${subtotal}</p>
+                    </div>
+                </Counter>
+            </div>
+        </ItemList>
+    )
+}
+
+
+const ItemList = styled.div`
+    width: 100%;
+    height: 80px;
+    border: 1px solid #000000;
+    border-radius: 5px;
+    padding: 10px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    div {
+        display: flex;
+        justify-content: flex-start;
+        align-items: center;
+        img {
+        width: 60px;
+        height: 60px;
+        border: 1px solid #000000;
+        border-radius: 5px;
+        margin-right: 10px;
+        }        
+    }    
+`
+
+const InfoItem = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    p {
+        font-size: 18px;
+        font-weight: 400;
+        color: #000000;
+    }
+`
+const Counter = styled.div`
+    border: 1px solid #000000;
+    border-radius: 5px;
+    display: flex;
+    justify-content: flex-end;
+    div {
+        width: 200px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-left: 10px;
+        p {
+            font-size: 20px;
+            font-weight: 700;
+            color: #000000;
+        }
+        
+    }
+`
+const CounterButton = styled.div`
+    display: flex;
+    justify-content: center;
+    div {
+        width: 20px;
+        height: 20px;
+        background-color: lightgray;
+        text-align: center;
+    }
+`
