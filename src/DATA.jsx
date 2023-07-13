640213d0a205f6abc0ec8409944993e7fdf09d94
THIAGO SOUSA <thiago21sousa21@gmail.com>
2023-07-13 12:02:00 -0300

    aumentando o arry de produtos 1.1

--- src/DATA.jsx
+++ src/DATA.jsx
@@ -113,7 +113,182 @@ const DATAPRODUCTS = [
             'https://cdn.leroymerlin.com.br/products/conjunto_para_jardim_aco_6_pecas_kangfu_champagne_90660654_a19f_140x140.jpeg',
             'https://cdn.leroymerlin.com.br/products/conjunto_para_jardim_aco_6_pecas_kangfu_champagne_90660654_50d6_600x600.jpeg'
         ]
-    }
+    },
+    {
+        name: "Jogo De Mesa 4 Poltronas Para Area Jardim Piscina Varanda",
+        description:`Sempre Pensando No Conforto E Bem-estar De Seus Clientes, A Sarah Móveis Disponibiliza Uma Grande Variedade De Produtos Para Deixar A Sua Área De Lazer Muito Mais Bonita. São Móveis Para Áreas Externas, Decoração, Jardins, Piscinas, Restaurantes, Hotéis, Espaço Gourmet E Até Mesmo Para Seu Apartamento. Com Um Rígido Controle De Qualidade Fornecemos Diversos Modelos Fabricados Em Alumínio Tipo Exportação. Todos Os Produtos São De Alta Qualidade, Beleza E Conforto.- Dados Técnicosestrutura Feita Em Perfil De Alumínio E Soldada Nos Processos Tig E Mig.acabamento Da Estrutura Feita Em Pintura Eletrostática Ou Polimento Podendo Ficar Em Áreas Externas, Não Enferruja.trançado Artesanalmente Em Fibra Sintética Altamente Resistente A Raios U.v Não Resseca E Nem Desbota.almofada Com Revestimento Em Tecido Acquablock Da Karsten Impermeável.- Incluso04 Poltronas01 Mesa 90cm Com Vidro- Atenção!!!antes De Realizar A Compra Pergunte Sobre O Valor Do Frete E Prazo De Entrega Informando Seu Cep Para Que Nossa Equipe Faça A Consulta.este Produto Não Faz Parte Dos Produtos Selecionados Para O Mercado Pontos Com Frete Grátis, Pois, Enviamos Somente Por Transportadora Ou Transporte Próprio.todas As Opções De Cores Serão Enviadas Após A Compra Para Escolha E Formalização Do Pedido.fabricação Própria E Garantia De 1 Ano Contra Defeitos De Fabricação.clique Em Comprar Apenas Se Tiver Certeza De Sua Compra E Já Souber O Valor Do Frete!!!!`,
+        value: 2264.51 ,
+        discount: 20,
+        sector: 'jardim',
+        images:[
+            'https://carrefourbr.vtexassets.com/arquivos/ids/47033180-540-auto?v=637866704695500000&width=540&height=auto&aspect=true',
+            
+        ]
+    },
+    {
+        name: "Conjunto Jardim em Junco 2 Poltronas e Mesa Centro Base Aço Ipanema - Café",
+        description:`O Conjunto Ipanema, é produzida com materiais de excelente qualidade, possui design moderno e sofisticado que combina perfeitamente nos mais diversos ambientes, proporcionando beleza para sua Área de lazer, Área de Churrasco, Apartamento, Balcão, Cozinha, Edícula, Espaço Gourmet, Sacada, Sala, Varanda ou onde desejar decorar.Tipo: Poltrona e Mesa de centro Modelo: IpanemaCor Poltrona: CaféCor Mesa de centro: CaféEstrutura em Aço carbono e pintura Eletrostática EpóxiAcabamento em Fibra Sintética com tratamento ANTI-UVEncosto com revestimento em fibra sintéticaAssento com revestimento em espuma e tecido impermeávelMedidas Poltrona:Altura: 88,5cmLargura: 55cmComprimento: 54,5cmMedidas Mesa de centro:Altura: 41cmLargura: 45cmComprimento: 45cmATENÇÃOIMAGENS MERAMENTE ILUSTRATIVAS REALIZADO EDIÇÃO, PODENDO HAVER UMA PEQUENA VARIAÇÃO DE COR DEPENDENDO DE CADA TELA OU MONITOR.
+        Marca: AJA FIBRAS`,
+        value: 1572.30 ,
+        discount: 20,
+        sector: 'jardim',
+        images:[
+            'https://imgs.casasbahia.com.br/1556534881/1xg.jpg?imwidth=64',
+            'https://imgs.casasbahia.com.br/1556534881/2xg.jpg?imwidth=64',
+            'https://imgs.casasbahia.com.br/1556534881/3xg.jpg?imwidth=64',
+            'https://imgs.casasbahia.com.br/1556534881/4xg.jpg?imwidth=64',
+        
+        ]
+    },
+    {
+        name: "Jogo Cadeiras E Mesa Jardim Área Externa - Varanda Móveis",
+        description:`Jogo de mesa Arcobalena
+
+        Composto por 1 mesa e 4 cadeiras com almofada.
+        
+        Obs: vidro não incluso
+        
+        Fabricado com estrutura reforçada em aço galvanizado tubular certificado, com solda mig.
+        
+        Revestimento feito artesanalmente em fibra sintética na cor Argila com tratamento UV resistente aos raios solares.
+        
+        Almofadas confeccionadas com tecidos da linha ACQUABLOCK da KARSTEN.
+        
+        Medidas da mesa:
+        Redonda 90 cm
+        Altura 80 cm
+        
+        Medidas das cadeiras:
+        Altura total 85 cm
+        Altura do assento 45 cm
+        Almofada de assento redonda 44 cm
+        Largura/Comprimento 57 cm
+        
+        Disponível em estoque
+        
+        Não contém frete grátis, consulte valores enviando o CEP de destino pelo campo de perguntas
+        
+        Tire todas suas dúvidas no campo de perguntas
+        
+        Produto de alta qualidade e fino acabamento garantido pelo fabricante`,
+        value: 1391,
+        discount: 20,
+        sector: 'jardim',
+        images:[
+            'https://http2.mlstatic.com/D_Q_NP_788212-MLB30794175106_052019-R.webp',
+            'https://http2.mlstatic.com/D_Q_NP_715245-MLB30794172123_052019-R.webp',
+            'https://http2.mlstatic.com/D_Q_NP_860696-MLB30794168689_052019-R.webp',
+        ]
+    },
+    {
+        name: "Conjunto para Jardim com Mesa Quadrada E Cadeiras Arredondadas 3 Peças Preto",
+        description:`O Conjunto de Jardim 2 Cadeiras e 1 Mesa Preto da Casanova proporciona luxo e simplicidade ao mesmo tempo, enquanto você toma seu café ou simplesmente passa um tempo descansando nesses móveis, deixando sua área externa ainda mais bonita e agradável.
+
+        A Casanova conta com toda qualidade, segurança e tolerância em seus produtos, que vão desde materiais essenciais para a construção de itens de decoração,`,
+        value: 509,
+        discount: 20,
+        sector: 'jardim',
+        images:[
+            'https://cec-a.akamaihd.net/img-prod/images/standard/conjunto-para-jardim-com-mesa-quadrada-e-cadeiras-arredondadas-3-pecas-preto-casanova-1385137-foto-20220216120928284_742680_A.png',
+            'https://cec-a.akamaihd.net/img-prod/images/standard/conjunto-para-jardim-com-mesa-quadrada-e-cadeiras-arredondadas-3-pecas-preto-casanova-1385137-foto-20220216120929673_742680_B.png',
+            
+        ]
+    },
+    {
+        name: "Conjunto Mesa E 4 Cadeiras Madeira Munik Furo Para Ombrelone",
+        description:`Atenção amigo cliente cote seu frete no campo de perguntas. Evite aborrecimentos.
+        Lindo conjunto formado por:
+        1 Mesa redonda, medindo 1.20comp. x 1.20larg. x 0.77alt
+        4 cadeiras empilháveis , medindo 470 X 600 X 800 cm
+        Nada se compara a nobreza da madeira, principalmente com consciência ecológica e respeito ao ambiente que vivemos.
+        Acabamento: Recebe tratamento em ECO PROTECTION®, que é um Stein impregnante e hidrorrepelente de ação cupinicida e fungicida. Padrão nogueira
+        
+        Produto semi-desmontado,acompanha manual de fácil entendimento.
+        Uso interno ou externo.`,
+        value: 1599,
+        discount: 20,
+        sector: 'jardim',
+        images:[
+            'https://http2.mlstatic.com/D_Q_NP_692709-MLB46026227795_052021-R.webp',
+            'https://http2.mlstatic.com/D_Q_NP_680317-MLB31219634422_062019-R.webp',
+            'https://http2.mlstatic.com/D_Q_NP_695228-MLB46026263153_052021-R.webp',
+            'https://http2.mlstatic.com/D_Q_NP_985464-MLB31857808432_082019-R.webp',
+        
+        ]
+    },
+    {
+        name: "Conjunto Maresia 6 Cadeiras Mesa Big Jardim Varanda Piscina",
+        description:`CONJUNTO DE MESA MARESIA - 6 CADEIRAS + MESA 1,05m- ALUMÍNIO E FIBRA SINTÉTICA
+        O conjunto Maresia,produzidoem alumínio e fibra sintética, traz toda a resistência e durabilidade que sua área externa precisa, álém do conforto e a elegância que seu ambiente merece!
+        Por ter a estrutura feita em alumínio, não enferruja, o que aumenta muitíssimo sua durabilidade. A fibra sintética utilizada na produção é tratada para resistir aos raios U.V., o que a torna resistente à ação do clima.
+        Obs. NÃO ACOMPANHA VIDRO!
+        
+        COMPRE COM QUEM ENTENDE DO ASSUNTO!
+        Há mais de 8 anos no mercado, a Realize Decor é referência na fabricação e lançamento de móveis em alumínio e fibra sintética para área externa e interna. Com a missão de levar conforto, beleza e praticidade aos nossos clientes, trabalhamos com material de altíssima qualidade, profissionais capacitados e, mais importante, o amor pelo que fazemos.
+        Prezamos pela satisfação do cliente, tanto com a qualidade do produto como no atendimento, estabelecendo uma relação de confiança, respeito e segurança.
+        
+        DIMENSÕES (média)
+        - CADEIRA:
+        Altura máxima: 80cm
+        Altura do chão ao assento: 43cm
+        Largura: 60cm
+        Profundidade total: 60cm
+        Profundidade assento: 42cm
+        
+        - MESA:
+        Altura: 70cm
+        Largura: 105cm
+        Obs. Mesa NÃO ACOMPANHA TAMPO DE VIDRO.
+        
+        ESPECIFICAÇÕES TÉCNICAS
+        Material da estrutura: Alumínio.
+        Solda: MIG, de alta resistência.
+        Material do acabamento: Fibra Sintética com tratamento ANTI-UV.
+        
+        GARANTIA
+        6 meses contra defeitos de fabricação.
+        
+        DIFERENCIAIS
+        - Fibra Sintética 100% Virgem (Maior resistência e durabilidade)
+        - Fibra com tratamento Anti-UV (Proteção contra Raios Ultravioleta)
+        
+        ***** VALOR REF. 06CADEIRAS MARESIA+ 01 MESA MARESIA 1,05m (SEM TAMPO)*****`,
+        value:1601 ,
+        discount: 20,
+        sector: 'jardim',
+        images:[
+            'https://http2.mlstatic.com/D_Q_NP_702972-MLB49274970311_032022-R.webp',
+        ]
+    },
+    {
+        name: "Conjunto Mesa Rústica Com Bancos Madeira Churrasco 2,00m - Mesas para Churrasco",
+        description:`Conjunto Mesa Rústica Com Bancos Madeira Churrasco 2,00m
+        Madeira 100% Eucalipto Maciço, mais beleza e durabilidade! Conjunto composto por Mesa + 02 Bancos 2,00m Fabricação em série padronizada, o que assegura uma qualidade superior ao produto! Medidas: Mesa: 2,00 x 0,80m Bancos: 2,00 x 0,30m Peso total: 70kg. Madeira de Verdade! Madeira: 100% Eucalipto Maciço Seco Acabamento: Selador Cerejeira ( Claro ) ou Tabaco ( Escuro ).`,
+        value: 2089.90 ,
+        discount: 20,
+        sector: 'jardim',
+        images:[
+            'https://a-static.mlcdn.com.br/800x560/conjunto-mesa-rustica-com-bancos-madeira-churrasco-200m-mesas-para-churrasco/mesasparachurrasco/me51/6201eafccb4281eb1a6a93a40c4ac62a.jpeg',
+            'https://a-static.mlcdn.com.br/90x90/conjunto-mesa-rustica-com-bancos-madeira-churrasco-200m-mesas-para-churrasco/mesasparachurrasco/me51/b59edda5d54dea7304e0d9ca3e82c955.jpeg',
+            'https://a-static.mlcdn.com.br/90x90/conjunto-mesa-rustica-com-bancos-madeira-churrasco-200m-mesas-para-churrasco/mesasparachurrasco/me51/e460b83bb8bc59a7ec90c10bae47232a.jpeg',
+            'https://a-static.mlcdn.com.br/90x90/conjunto-mesa-rustica-com-bancos-madeira-churrasco-200m-mesas-para-churrasco/mesasparachurrasco/me51/c6736b75b91af3ddbda5c9f04cccb062.jpeg',
+        
+        ]
+    },
+    // {
+    //     name: "",
+    //     description:'',
+    //     value: ,
+    //     discount: 20,
+    //     sector: 'jardim',
+    //     images:[
+    //         '',
+    //         '',
+    //         '',
+    //         '',
+    //         ''
+    //     ]
+    // },
 
 ];
 
