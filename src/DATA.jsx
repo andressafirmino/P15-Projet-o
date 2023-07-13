2697e304e5860cbfde2a044a200572f767230c5e
THIAGO SOUSA <thiago21sousa21@gmail.com>
2023-07-13 12:30:47 -0300

    aumentando o arry de produtos 1.2

--- src/DATA.jsx
+++ src/DATA.jsx
@@ -1,12 +1,4 @@
-// {
-//     name: "",
-//     description:'',
-//     value: ,
-//     discount: ,
-//     sector: '',
-//     images:[
-//     ]
-// }
+
 
 const DATAPRODUCTS = [
     {
@@ -275,12 +267,353 @@ const DATAPRODUCTS = [
         
         ]
     },
+    {
+        name: "Mesa para reunião Evolution",
+        description:`Mesa para reunião Evolution
+        Mesa para reunião Evolution`,
+        value: 4700,
+        discount: 0,
+        sector: 'reuniao',
+        images:[
+            'https://a-static.mlcdn.com.br/800x560/mesa-para-reuniao-evolution/regilaneoffice/042c1ea0c7ec11ecbd8d4201ac18506b/0ce9fbf6dbfdf981b87737d0e8a8cfa3.jpeg',
+           
+        ]
+    },
+    {
+        name: "Mesa de Reunião Premiatto Retangular",
+        description:`Mesa de Reunião Premiatto Retangular
+
+        Tampo sobreposto, em tamburato de 40mm, filetado em todos os lados com filete de 2,5mm.
+        Painel Frontal em BP 18MM, fixado por minifix, com detalhe central em tubo 50 x 20 mm.
+        Pés Laterais, em tamburato 54mm, filetado em todos os lados com filete de 2,5mm e fixado ao tampo por distanciadores metálicos 50 x 50mm.
+        
+        Disponível nas medidas:
+        2000(L) x 1000(P) x 740(A)mm
+        2400(L) x 1200(P) x 740(A) mm
+        
+        
+        Consulte nosso departamento de televendas para mais opções de tamanhos, modelos, cores, etc`,
+        value: 3829.14,
+        discount: 20,
+        sector: 'reuniao',
+        images:[
+            'https://d2r9epyceweg5n.cloudfront.net/stores/001/610/404/products/reu-fl-premiatto-mes1-d2b4aa42952e80d88d16248036604731-640-0.jpg',
+        
+        ]
+    },
+    {
+        name: "Mesa de Reunião Prius",
+        description:`Mesa de Reunião Prius
+
+        Tampo confeccionado em MDP 40mm engrossurado.
+        Pés painel de 25mm.
+        Caixa de Tomadas para elétrica e dados em metal
+        Cores opcionais:
+        
+        Ameixa/preto, Nogal/preto, calvi/preto, marsalha/preto, carvalho berlim/preto
+        
+        disponível nas medidas:
+        2000(L) X 1000(P) X 745(A)mm - com 1 caixa de tomadas para eletrica e dados
+        2700(L) X 1000(P) X 745(A)mm - com 2 caixas de tomadas para eletrica e dados`,
+        value:1741.91 ,
+        discount: 20,
+        sector: 'reuniao',
+        images:[
+            'https://d2r9epyceweg5n.cloudfront.net/stores/001/610/404/products/reu-gb-prius-11-3123efcf55549d4da116248037021792-640-0.jpg',
+            'https://d2r9epyceweg5n.cloudfront.net/stores/001/610/404/products/reu-gb-prius-21-fbfbd7ee8a37b0ac2c16248037022244-640-0.jpg',
+    
+        ]
+    },
+    {
+        name: "Mesa de Reunião Atualle Semi Oval",
+        description:`Mesa de Reunião Atualle Semi Oval
+        Tampo Semi Oval, confeccionado em melamínico BP 25mm, pés painel 25mm, com fita de borda 2mm em todo contorno, com calha de subida para fiação.
+        Painel frontal BP: Confeccionado em melamínico BP 25mm.
+        Cores Disponíveis: Cristal, argila, branco, preto, grafite, Alamo, Lanarca, Itapuá, Carvalho.
+        Disponível nas medidas:
+        1600(L) x 900(P) x 740(A)mm - com 1 caixa de tomada para eletrica e dados
+        2000(L) x 900(P) x 740(A)mm - com 1 caixa de tomada para eletrica e dados
+        2200(L) x 1000(P) x 740(A)mm - com 1 caixa de tomada para eletrica e dados
+        2500(L) x 1100(P) x 740(A)mm - com 1 caixa de tomada para eletrica e dados
+        2700(L) X 1100(P) x 740(A)mm - com 1 caixa de tomada para eletrica e dados
+        3000(L) x 1100(P) x 740(A)mm - Tampo bi-partido - com 2 caixas de tomada para eletrica e dados
+        3500(L) x 1200(P) x 740(A)mm - Tampo bi-partido - com 2 caixas de tomada para eletrica e dados`,
+        value: 1754.30,
+        discount: 20,
+        sector: 'reuniao',
+        images:[
+            'https://d2r9epyceweg5n.cloudfront.net/stores/001/610/404/products/reu-mv-msopn2511-11-8648f08e2f19b6843416248035005415-640-0.jpg',
+            'https://d2r9epyceweg5n.cloudfront.net/stores/001/610/404/products/reu-mv-msopn2511-21-17ca1a75c7bc28129416248035007532-640-0.jpg',
+    
+        ]
+    },
+    {
+        name: "Mesa de Reunião 40 Retangular",
+        description:`Mesa de Reunião 40 Retangular
+        Tampo confeccionado em BP 40mm (engrossado)
+        Pés painel de 25mm.
+        Painel frontal de 15mm.
+        Caixas de tomada em ço para elétrica e dados
+        Disponível nas medidas:
+        
+        1600(L) x 900(P) x 740(A) mm - Tampo único com 1 caixa de tomadas para elétrica e dados
+        
+        2000(L) x 900(P) x 740(A) mm - Tampo único com 1 caixa de tomadas para elétrica e dados
+        
+        2500(L) x 1000(P) x 740(A) mm - Tampo único com 1 caixa de tomadas para elétrica e dados
+        
+        3000(L) x 1200(P) x 740(A) mm - Tampo Bi-partido com 1 caixa de tomadas para elétrica e dados
+        
+        3000(L) x 1200(P) x 740(A) mm - Tampo Bi-partido com 2 caixas de tomadas para elétrica e dados`,
+        value: 1006,
+        discount: 20,
+        sector: 'reuniao',
+        images:[
+            'https://d2r9epyceweg5n.cloudfront.net/stores/001/610/404/products/mv-mesa-reuniao-retangular-impact-401-d4963e1f6d0bc2345016354560679996-480-0.jpg',
+           
+        ]
+    },
+    {
+        name: "Mesa Reunião Redonda Boss Montana - Plata",
+        description:`
+        Dimensões do produto:
+        
+        Altura: 75 cm
+        
+        Largura: 120 cm
+        
+        Profundidade: 120 cm
+        
+        Informações do produto:
+        
+        Gaveta: Não.
+        
+        Porta: Não.
+        
+        Pés: Sim, pés em aço com pintura EPOXI na cor Preto.
+        
+        Cor: Montana
+        Material principal: MDP
+        
+        Itens inclusos:
+        
+        01 Mesa Redonda de Reunião.
+        
+        Kit completo para montagem, manual de instruções para montagem.
+        
+        Garantia:
+        
+        3 meses contra defeito de fabricação.
+        
+        Informações complementares:
+        
+        - Fotos meramente ilustrativas do produto no ambiente.
+        
+        - Produto não acompanha cadeiras.
+        
+        - Os objetos que ambientam as fotos não acompanham o produto;
+        
+        - Verificar descrição e tamanho do produto antes da compra.
+        
+        - Avaliar a mercadoria no ato do recebimento e recusar em caso de divergência ou avarias.
+        
+        - Mantenha as informações de contato e endereço de entrega atualizados para entrarmos em contato caso necessário.
+        
+        - Limpar com flanela ou pano macio seco, não utilizar produtos abrasivos, desengordurantes, álcool ou solvente. Não expor o produto diretamente à luz solar e à umidade.
+        
+        * Sugerimos a contratação de um montador profissional para a montagem do produto. Não nos responsabilizamos por avarias causadas no ato da montagem.
+        
+        * Não nos responsabilizamos pela montagem e desmontagem do produto no ato da entrega e devolução, assim como os custos gerados nestas situações.
+        
+        * A transportadora entregará o produto no andar térreo do endereço de entrega.
+        
+        * Não nos responsabilizamos por subir escadas, corredores e elevadores ou transporte por guincho em apartamentos.
+        Nome Via Varejo - CasaH
+        Mesa Reunião Redonda Boss
+        EAN - Mais de Casa
+        7908458742547
+        Nome - Mais de Casa
+        Mesa Reunião Redonda Dubai Montana
+        Nome Via Varejo - Mais de Casa
+        Mesa Reunião Redonda Dubai
+        Ignore Marketplace?
+        0
+        Altura
+        De 70 cm a 85 cm
+        Largura
+        Acima de 130 cm
+        Profundidade
+        De 85 cm a 100 cm
+        Gavetas
+        Não
+        Material
+        MDP
+        Gamer
+        Não
+        NovaPC Sku Fabricante
+        1443.12
+        NovaPC Preço de Custo
+        267.84100000000001
+        nbm
+        nacional
+        Informações complementares
+        Cor	Marrom
+        Formato	Redondo`,
+        value: 651.47 ,
+        discount: 20,
+        sector: 'reuniao',
+        images:[
+            'https://a-static.mlcdn.com.br/90x90/mesa-reuniao-redonda-boss-montana-plata/casah/pla202272/c09fc0a6a75cb5c6a399652b4051db5b.jpeg',
+            'https://a-static.mlcdn.com.br/90x90/mesa-reuniao-redonda-boss-montana-plata/casah/pla202272/44026fcfe83bc9e5662c2d51474cd76b.jpeg',
+            'https://a-static.mlcdn.com.br/90x90/mesa-reuniao-redonda-boss-montana-plata/casah/pla202272/a97ab27229355e500a8878ec4428838e.jpeg',
+            
+        ]
+    },
+    {
+        name: "",
+        description:``,
+        value: ,
+        discount: 20,
+        sector: 'reuniao',
+        images:[
+            '',
+            '',
+            '',
+            '',
+            ''
+        ]
+    },
+    {
+        name: `Conjunto de Mesa 1,20x70 e Mesa Aparador Dobrável com 6 Cadeiras - Tabaco - PREGUIÇOSA
+        `,
+        description:`Conjunto de Mesa 1,20x70 e Mesa Aparador Dobrável com 6 Cadeiras - Tabaco
+        Conjunto De Mesa 1,20x70 E Mesa Aparador Dobrável Com 6 Cadeiras - Tabaco
+        Feito em madeira maciça, tornando resistentes e duráveis. Seu design permite que seja encaixado em qualquer ambiente, trazendo um toque a mais para seu estabelecimento. É ideal para bares, restaurantes, estabelecimentos, áreas de lazer e também para sua casa. Feito em madeira maciça, tornando resistentes e duráveis. Seu design permite que seja encaixado em qualquer ambiente, trazendo um toque a mais para seu estabelecimento. Além de sua durabilidade, o que lhe trará conforto para servir seus clientes. Por ser dobrável torna-se mais fácil guardar, manusear e limpar o produto.
+        
+        Tipo: Conjunto
+        Modelo: Dobrável
+        Cor: Tabaco
+        
+        Medidas Aparador:
+        
+        Altura: 75cm
+        Largura: 35cm
+        Comprimento: 70cm
+        
+        Medidas Mesa:
+        
+        Altura: 75cm
+        Largura: 70cm
+        Comprimento: 120cm
+        
+        Medidas Cadeira:
+        
+        Altura: 80cm
+        Largura: 45cm
+        Comprimento: 42cm
+        
+        OBS: Evite usar produtos abrasivos e químicos para limpeza do produto, aconselha-se o uso de pano umedecido para melhor conservação; mantenha o produto fora de chuva e sol constantes.`,
+        value: 857.69,
+        discount: 20,
+        sector: 'reuniao',
+        images:[
+            'https://a-static.mlcdn.com.br/90x90/conjunto-de-mesa-120x70-e-mesa-aparador-dobravel-com-6-cadeiras-tabaco-preguicosa/preguicosa/570539/0db7654799c2694c7ee5fbc4ea9b033a.jpeg',
+            'https://a-static.mlcdn.com.br/90x90/conjunto-de-mesa-120x70-e-mesa-aparador-dobravel-com-6-cadeiras-tabaco-preguicosa/preguicosa/570539/66a4334ae4f1ddb4c9c3c121513dd6fe.jpeg',
+            'https://a-static.mlcdn.com.br/90x90/conjunto-de-mesa-120x70-e-mesa-aparador-dobravel-com-6-cadeiras-tabaco-preguicosa/preguicosa/570539/50eda4b1a1191023aeb0654c92daf33e.jpeg',
+           
+        ]
+    },
+    {
+        name: `Mesa de Reunião 2.0 Ret Afarra - Plata
+        `,
+        description:`Mesa de Reunião 2.0 Ret Afarra
+        A Mesa trás praticidade e produtividade ao realizar seu trabalho.
+        
+        Em MDP BP ou EUCAPRINT 15 mm
+        
+        Acabamento no Tampo com Perfil em PVC.
+        
+        Pés em aço com Niveladores de Altura, pintura EPOXI nas cores Cinza ou Preto.
+        
+        NÃO ACOMPANHA CADEIRA
+        
+        Dimensões do produto:
+        
+        Altura: 75 cm
+        
+        Largura: 200 cm
+        
+        Profundidade: 90 cm
+        
+        Informações do produto:
+        
+        Gaveta: Não.
+        
+        Porta: Não.
+        
+        Pés: Sim, Pés na Cor Preto com Niveladores de Altura
+        
+        Cor: Affara
+        
+        Cadeiras: Não
+        
+        Material principal: MDP BP 15 mm
+        
+        Itens inclusos:
+        
+        01 Mesa de Reunião.
+        
+        Kit completo para montagem, manual de instruções para montagem.`,
+        value: 870.42,
+        discount: 20,
+        sector: 'reuniao',
+        images:[
+            'https://a-static.mlcdn.com.br/90x90/mesa-de-reuniao-2-0-ret-afarra-plata/casah/pla1455243/97afc1e2ae6d0b965508f4a86ff1341f.jpeg',
+            'https://a-static.mlcdn.com.br/90x90/mesa-de-reuniao-2-0-ret-afarra-plata/casah/pla1455243/708490104e2ea0b136b8880861cdca37.jpeg',
+            'https://a-static.mlcdn.com.br/90x90/mesa-de-reuniao-2-0-ret-afarra-plata/casah/pla1455243/c8412057a93dc4b814340f1036a0b8c8.jpeg',
+        ]
+    },
+    {
+        name: `Mesa para Escritório de Reuniões 220cm Office Kappesberg Carvalho/Munique`,
+        description:`Mesa para Escritório de Reuniões 220cm Office Kappesberg Carvalho/Munique
+        Pesos e Dimensões
+        Altura (cm)	77 cm
+        Largura (cm)	220 cm
+        Profundidade (cm)	91 cm
+        Peso (kg)	46.82 kg
+        Garantia
+        Garantia	03 Meses
+        Materiais e Acabamentos
+        Material Principal	MDP
+        Acabamento	Pintura Touch UV
+        Escala de Brilho	Fosco
+        Material dos Pés	Metal
+        Acabamento dos Pés	Pintura
+        Material do Puxador	Metal
+        Montagem
+        Necessita Montagem	Sim
+        Complexidade da Montagem	Baixa: pode ser montado por uma pessoa sozinha
+        Sistema de Montagem	Parafuso Philips
+        Recomendações de Uso, Manutenção e Limpeza	Use um pano limpo e ligeiramente umedecido em água ou álcool (recomendamos álcool etílico hidratado). Logo após, seque a superfície com um pano seco e macio.
+        Itens Inclusos
+        Acompanha Manual de Instalação	Sim
+        Itens Inclusos	1 Mesa e 1 Manual de Montagem`,
+        value: 772.79,
+        discount: 20,
+        sector: 'reuniao',
+        images:[
+            'https://product-hub-prd.madeiramadeira.com.br/framereuniao.jpg/images/framereuniao.jpg?width=700&canvas=1:1&bg-color=FFF',
+            'https://product-hub-prd.madeiramadeira.com.br/framereuniao.jpg/images/framereuniao.jpg?width=700&canvas=1:1&bg-color=FFF',
+            'https://product-hub-prd.madeiramadeira.com.br/184554/images/b17861e7-f53f-40ff-8f7f-118df829d65facca1f95b49b1a33e211845541.jpg?width=700&canvas=1:1&bg-color=FFF',
+            
+        ]
+    },
     // {
     //     name: "",
-    //     description:'',
+    //     description:``,
     //     value: ,
     //     discount: 20,
-    //     sector: 'jardim',
+    //     sector: 'reuniao',
     //     images:[
     //         '',
     //         '',
